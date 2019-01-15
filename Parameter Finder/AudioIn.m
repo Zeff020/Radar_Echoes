1b50262230e8def45232546cd5a8735eeaa2aa20
alisidley <alexsidley@gmail.com>
2019-01-15 08:00:52 +0100

    idk

--- Parameter Finder/AudioIn.m
+++ Parameter Finder/AudioIn.m
@@ -1,5 +1,6 @@
 function [S,f,t,hop,fs] = AudioIn(AudioFileName)
 % load an audio file
+AudioFileName = "./AudioFiles/" + AudioFileName;
 [x, fs] = audioread(AudioFileName);   % load an audio file
 x = x(1:2000000, 1);                        % get the first channel
 % define analysis parameters
